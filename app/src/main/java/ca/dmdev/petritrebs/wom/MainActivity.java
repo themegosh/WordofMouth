d3baccbfa6d11b2adcbfd3f3239adb3e375f5ae5
themegosh <mathewdf@gmail.com>
2016-02-12 04:48:33 -0500

    Implement Places Adapter for searching!

--- app/src/main/java/ca/dmdev/petritrebs/wom/MainActivity.java
+++ app/src/main/java/ca/dmdev/petritrebs/wom/MainActivity.java
@@ -6,14 +6,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.text.Html;
+import android.text.Spanned;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -25,11 +29,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.*;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapsInitializer;
@@ -37,10 +53,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String[] INITIAL_PERMS={
         android.Manifest.permission.ACCESS_FINE_LOCATION,
@@ -50,7 +68,7 @@ public class MainActivity extends AppCompatActivity
     private static final int LOCATION_REQUEST=INITIAL_REQUEST+3;
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
 
-    private static final String TAG = LoginActivity.class.getName();
+    private static final String TAG = MainActivity.class.getName();
     private SlidingUpPanelLayout mLayout;
     private Toolbar toolbar;
     private FloatingActionButton fabAddLocation;
@@ -58,6 +76,12 @@ public class MainActivity extends AppCompatActivity
     private GoogleMap map;
     private LocationManager locationManager;
     private FragmentManager fragmentManager;
+    private TextView lblPlaceTitle;
+    private TextView lblScrollView;
+
+    protected GoogleApiClient mGoogleApiClient;
+    private PlaceAutocompleteAdapter mAdapter;
+    private AutoCompleteTextView mAutocompleteView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +97,7 @@ public class MainActivity extends AppCompatActivity
         initializeFab();
         initializeNavPanel();
         initializeMap();
+        initializePlacesApi();
     }
 
     @Override
@@ -107,7 +132,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_filter) {
+        if (id == R.id.btnClearToolbar) {
+            mAutocompleteView.setText("");
             return true;
         }
 
@@ -191,6 +217,23 @@ public class MainActivity extends AppCompatActivity
             // other 'case' lines to check for other
             // permissions this app might request
         }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        this.map = map;
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+        }
+        map.setTrafficEnabled(true);
+        map.setIndoorEnabled(true);
+        map.setBuildingsEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
+
+        centerMapOnMyLocation();
     }
 
     private void initializePermissions(){
@@ -292,23 +335,26 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    @Override
-    public void onMapReady(GoogleMap map) {
-        this.map = map;
-        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    private void initializePlacesApi(){
+        LatLngBounds BOUNDS_TORONTO = new LatLngBounds(
+                new LatLng(43.326385, -78.873712), new LatLng(43.946721, -80.103337));
 
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            map.setMyLocationEnabled(true);
-        }
-        map.setTrafficEnabled(true);
-        map.setIndoorEnabled(true);
-        map.setBuildingsEnabled(true);
-        map.getUiSettings().setZoomControlsEnabled(true);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, 0 /* clientId */, this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
 
-        centerMapOnMyLocation();
+        mAutocompleteView = (AutoCompleteTextView)
+                findViewById(R.id.autocomplete_places);
+        mAutocompleteView.setOnItemClickListener(mAutocompleteClickListener);
+
+        mAdapter = new PlaceAutocompleteAdapter(this, mGoogleApiClient, BOUNDS_TORONTO,
+                null);
+        mAutocompleteView.setAdapter(mAdapter);
+
+        lblPlaceTitle = (TextView) findViewById(R.id.lblPlaceTitle);
+        lblScrollView = (TextView) findViewById(R.id.lblScrollView);
     }
-
 
     private void centerMapOnMyLocation(){
         Location location = getMyLocation();
@@ -351,5 +397,135 @@ public class MainActivity extends AppCompatActivity
             return null;
         }
     }
+
+    /**
+     * Listener that handles selections from suggestions from the AutoCompleteTextView that
+     * displays Place suggestions.
+     * Gets the place id of the selected item and issues a request to the Places Geo Data API
+     * to retrieve more details about the place.
+     *
+     * @see com.google.android.gms.location.places.GeoDataApi#getPlaceById(com.google.android.gms.common.api.GoogleApiClient,
+     * String...)
+     */
+    private AdapterView.OnItemClickListener mAutocompleteClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            /*
+             Retrieve the place ID of the selected item from the Adapter.
+             The adapter stores each Place suggestion in a AutocompletePrediction from which we
+             read the place ID and title.
+              */
+            final AutocompletePrediction item = mAdapter.getItem(position);
+            final String placeId = item.getPlaceId();
+            final CharSequence primaryText = item.getPrimaryText(null);
+
+            Log.i(TAG, "Autocomplete item selected: " + primaryText);
+
+            /*
+             Issue a request to the Places Geo Data API to retrieve a Place object with additional
+             details about the place.
+              */
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+
+            Toast.makeText(getApplicationContext(), "Clicked: " + primaryText,
+                    Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "Called getPlaceById to get Place details for " + placeId);
+        }
+    };
+
+    /**
+     * Callback for results from a Places Geo Data API query that shows the first place result in
+     * the details view on screen.
+     */
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                // Request did not complete successfully
+                Log.e(TAG, "Place query did not complete. Error: " + places.getStatus().toString());
+                places.release();
+                return;
+            }
+            // Get the Place object from the buffer.
+            final com.google.android.gms.location.places.Place place = places.get(0);
+
+            //remove the keyboard from the screen
+            mAutocompleteView.clearFocus();
+            //hide keyboard
+            InputMethodManager imm = (InputMethodManager) getBaseContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(mAutocompleteView.getWindowToken(), 0);
+
+            // Format details of the place for display and show it in a TextView.
+            lblPlaceTitle.setText("Name: "+ place.getName());
+            //formatPlaceDetails(getResources(), place.getName(), place.getId(), place.getAddress(), place.getPhoneNumber(),                    place.getWebsiteUri()));
+            lblScrollView.setText("ID: " + place.getId() + "\nAddress: " + place.getAddress() + "\nLat/Lang: " + place.getLatLng().toString() + "\nPhone: " + place.getPhoneNumber() + "\nWebsite: " + place.getWebsiteUri().toString());
+
+            //anchor the panel
+            mLayout.setAnchorPoint(0.7f);
+            mLayout.setPanelState(SlidingUpPanelLayout.PanelState.ANCHORED);
+
+            //move the map
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(place.getLatLng().latitude, place.getLatLng().longitude))      // Sets the center of the map to location user
+                    .zoom(14)                   // Sets the zoom
+                    .bearing(0)                // Sets the orientation of the camera to north
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            //add a marker
+            map.addMarker(new MarkerOptions()
+                    .position(place.getLatLng())
+                    .title(place.getName().toString()));
+
+            // Display the third party attributions if set.
+            /*final CharSequence thirdPartyAttribution = places.getAttributions();
+            if (thirdPartyAttribution == null) {
+                lblPlaceAddress.setVisibility(View.GONE);
+            } else {
+                lblPlaceAddress.setVisibility(View.VISIBLE);
+                lblPlaceAddress.setText(Html.fromHtml(thirdPartyAttribution.toString()));
+            }*/
+
+            Log.i(TAG, "Place details received: " + place.getName());
+
+            places.release();
+        }
+    };
+
+    /**
+     * Called when the Activity could not connect to Google Play services and the auto manager
+     * could resolve the error automatically.
+     * In this case the API is not available and notify the user.
+     *
+     * @param connectionResult can be inspected to determine the cause of the failure
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        Log.e(TAG, "onConnectionFailed: ConnectionResult.getErrorCode() = "
+                + connectionResult.getErrorCode());
+
+        // TODO(Developer): Check error code and notify the user of error state and resolution.
+        Toast.makeText(this,
+                "Could not connect to Google API Client: Error " + connectionResult.getErrorCode(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
 
 }
