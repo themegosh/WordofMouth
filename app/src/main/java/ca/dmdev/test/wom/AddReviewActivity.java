0362056f8064e5f00355ea5a3968a4286c84c985
themegosh <mathewdf@gmail.com>
2016-03-05 03:27:34 -0500

    fix issue with nav back not working

--- app/src/main/java/ca/dmdev/test/wom/AddReviewActivity.java
+++ app/src/main/java/ca/dmdev/test/wom/AddReviewActivity.java
@@ -1,60 +1,66 @@
 package ca.dmdev.test.wom;
 
 import android.content.DialogInterface;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AddReviewActivity extends AppCompatActivity {
 
+    private static final String TAG = WordOfMouth.class.getName();
+
     Toolbar toolbar;
     DialogInterface.OnClickListener dialogBackConfirm;
+    WordOfMouth wom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_review);
+        wom = (WordOfMouth)getApplication();
 
         toolbar = (Toolbar) findViewById(R.id.add_review_toolbar);
-        toolbar.setTitle("Review: asd");
+        toolbar.setTitle("Review: "+wom.getSelectedPlace().getName());
 
-
-
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialogBackConfirm = new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        switch (which){
-                            case DialogInterface.BUTTON_POSITIVE:
-                                finish();
-                                break;
-
-                            case DialogInterface.BUTTON_NEGATIVE:
-                                //do nothing
-                                break;
-                        }
-                    }
-                };
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(AddReviewActivity.this);
-                builder.setMessage("Are you sure you want to discard this review?")
-                        .setPositiveButton("Yes", dialogBackConfirm)
-                        .setNegativeButton("No", dialogBackConfirm)
-                        .show();
-            }
-        });
         setSupportActionBar(toolbar);
 
-        try {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-        } catch (Exception e) {
-            e.printStackTrace();
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+            ab.setDisplayShowHomeEnabled(true);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == android.R.id.home) {
+            dialogBackConfirm = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which){
+                        case DialogInterface.BUTTON_POSITIVE:
+                            finish();
+                            break;
+
+                        case DialogInterface.BUTTON_NEGATIVE:
+                            //do nothing
+                            break;
+                    }
+                }
+            };
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(AddReviewActivity.this);
+            builder.setMessage("Are you sure you want to discard this review?")
+                    .setPositiveButton("Yes", dialogBackConfirm)
+                    .setNegativeButton("No", dialogBackConfirm)
+                    .show();
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 
     @Override
